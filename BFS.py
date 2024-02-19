ade2286402728a4a53074505114c445d67a912cb
HarGrif <160345070+HarGrif@users.noreply.github.com>
2024-02-19 18:44:34 +0000

    Merge branch 'Pathfinding'

--- BFS.py
+++ BFS.py
@@ -14,6 +14,7 @@ goal = np.array([24, 29])
 def bfs(bfs_map, bfs_goal, bfs_start):
     solving = True
     pathfinding = True
+    bfs_path = np.array([])
     m_rows = len(bfs_map)
     m_cols = len(bfs_map[0])
     search = np.zeros([m_rows, m_cols])
@@ -64,14 +65,42 @@ def bfs(bfs_map, bfs_goal, bfs_start):
             bfs_path = None
             print("Unsolvable")
 
+    next_index = np.array([bfs_goal[0], bfs_goal[1]])
+    while pathfinding:
+        dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+        dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+        smallest = float('inf')  # Initialize smallest with positive infinity
+        index = next_index
+        print(index)
+        for i in range(8):
+            new_x = index[1] + dx[i]
+            new_y = index[0] + dy[i]
+            # Ensure coordinates are within bounds of the matrix
+            if 0 <= new_x < m_cols and 0 <= new_y < m_rows:
+                value = search[new_y, new_x]
+                # Update minimum if found a smaller value that is not zero
+                if value != 0 and value < smallest:
+                    smallest = value
+                    next_index = np.array([new_y, new_x])
+                    if smallest == 1:
+                        np.append(bfs_path, start)
+                        pathfinding = False
+                        continue
+        np.append(bfs_path, index)
+
+    print(bfs_path)
+
+    plt.figure()
     plt.imshow(search, cmap='binary', interpolation='nearest')
+    plt.plot(bfs_path[:, 1], bfs_path[:, 0], marker='o', color='red')
     plt.show()
 
     return bfs_path
 
 
-plt.imshow(map, cmap='binary', interpolation='nearest')
-plt.show()
+# plt.imshow(map, cmap='binary', interpolation='nearest')
+# plt.show()
 
 bfs(map, goal, start)
 
