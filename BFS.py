521774ff8787df0e327e8022a7f4099f81c5d12b
HarGrif <160345070+HarGrif@users.noreply.github.com>
2024-02-20 11:16:16 +0000

    Solves for shortest path and returns path
    To Do:
    - Fix for not solvable

--- BFS.py
+++ BFS.py
@@ -1,20 +1,11 @@
 import numpy as np
 from copy import deepcopy
-import matplotlib.pyplot as plt
-
-map = np.zeros([25, 30])
-map[20, 0:10] = 1
-map[20, 19:30] = 1
-map[8, 4:20] = 1
-map[9:25, 16] = 1
-start = np.array([24, 1])
-goal = np.array([24, 29])
 
 
 def bfs(bfs_map, bfs_goal, bfs_start):
     solving = True
     pathfinding = True
-    bfs_path = np.array([])
+    bfs_path = []
     m_rows = len(bfs_map)
     m_cols = len(bfs_map[0])
     search = np.zeros([m_rows, m_cols])
@@ -72,36 +63,28 @@ def bfs(bfs_map, bfs_goal, bfs_start):
 
         smallest = float('inf')  # Initialize smallest with positive infinity
         index = next_index
-        print(index)
         for i in range(8):
             new_x = index[1] + dx[i]
             new_y = index[0] + dy[i]
-            # Ensure coordinates are within bounds of the matrix
+            # Ensure coordinates are within matrix
             if 0 <= new_x < m_cols and 0 <= new_y < m_rows:
                 value = search[new_y, new_x]
-                # Update minimum if found a smaller value that is not zero
+                # Update minimum if smaller value found
                 if value != 0 and value < smallest:
                     smallest = value
                     next_index = np.array([new_y, new_x])
+                    # Check if goal start has been reached
                     if smallest == 1:
-                        np.append(bfs_path, start)
                         pathfinding = False
                         continue
-        np.append(bfs_path, index)
+        bfs_path.append(index)
 
-    print(bfs_path)
+    # Convert list of arrays to 2D array with format [[y1, y2, y3, ... ,yn],[x1, x2, x3, ... ,xn]]
+    bfs_path.append(bfs_start)
+    fin_path = np.zeros([2, len(bfs_path)])
+    for i in range(len(bfs_path)):
+        fin_path[0, i] = bfs_path[i][0]
+        fin_path[1, i] = bfs_path[i][1]
+    print(fin_path)
 
-    plt.figure()
-    plt.imshow(search, cmap='binary', interpolation='nearest')
-    plt.plot(bfs_path[:, 1], bfs_path[:, 0], marker='o', color='red')
-    plt.show()
-
-    return bfs_path
-
-
-# plt.imshow(map, cmap='binary', interpolation='nearest')
-# plt.show()
-
-bfs(map, goal, start)
-
-#just for the commit
+    return fin_path
