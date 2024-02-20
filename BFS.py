357994e6cc8afa6392b09d7b044465e98709ccfe
HarGrif <160345070+HarGrif@users.noreply.github.com>
2024-02-20 11:53:14 +0000

    Functionality is complete - two examples added

--- BFS.py
+++ BFS.py
@@ -3,11 +3,14 @@ from copy import deepcopy
 
 
 def bfs(bfs_map, bfs_goal, bfs_start):
+    solvable = True
     solving = True
     pathfinding = True
     bfs_path = []
     m_rows = len(bfs_map)
     m_cols = len(bfs_map[0])
+
+    # Initialise search point
     search = np.zeros([m_rows, m_cols])
     search[bfs_start[0], bfs_start[1]] = 1
     step = 0
@@ -20,8 +23,9 @@ def bfs(bfs_map, bfs_goal, bfs_start):
         for i in range(len(indices[0])):
             index = np.array([indices[0][i], indices[1][i]])
 
+            # Check indices are within matrix bounds
+
             # For cell to the left
-            # Check if the indices are within the bounds of the matrix
             if 0 <= index[0] - 1 < m_rows and 0 <= index[1] < m_cols:
                 # Check if squares have been stepped on before or if the map has an obstacle there
                 if search[index[0] - 1, index[1]] == 0 and bfs_map[index[0] - 1, index[1]] == 0:
@@ -48,43 +52,44 @@ def bfs(bfs_map, bfs_goal, bfs_start):
                 if search[index[0], index[1] + 1] == 0 and bfs_map[index[0], index[1] + 1] == 0:
                     # Take step
                     search[index[0], index[1] + 1] = step + 1
-
+        if np.array_equal(search, prev_step):
+            solvable = False
+            solving = False
         if search[bfs_goal[0], bfs_goal[1]] == 0:
             continue
         solving = False
-        if np.array_equal(search, prev_step):
-            bfs_path = None
-            print("Unsolvable")
 
-    next_index = np.array([bfs_goal[0], bfs_goal[1]])
-    while pathfinding:
-        dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-        dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+    if solvable:
+        next_index = np.array([bfs_goal[0], bfs_goal[1]])
+        while pathfinding:
+            dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+            dy = [-1, 0, 1, -1, 1, -1, 0, 1]
 
-        smallest = float('inf')  # Initialize smallest with positive infinity
-        index = next_index
-        for i in range(8):
-            new_x = index[1] + dx[i]
-            new_y = index[0] + dy[i]
-            # Ensure coordinates are within matrix
-            if 0 <= new_x < m_cols and 0 <= new_y < m_rows:
-                value = search[new_y, new_x]
-                # Update minimum if smaller value found
-                if value != 0 and value < smallest:
-                    smallest = value
-                    next_index = np.array([new_y, new_x])
-                    # Check if goal start has been reached
-                    if smallest == 1:
-                        pathfinding = False
-                        continue
-        bfs_path.append(index)
+            smallest = float('inf')  # Initialize smallest with positive infinity
+            index = next_index
+            for i in range(8):
+                new_x = index[1] + dx[i]
+                new_y = index[0] + dy[i]
+                # Ensure coordinates are within matrix
+                if 0 <= new_x < m_cols and 0 <= new_y < m_rows:
+                    value = search[new_y, new_x]
+                    # Update minimum if smaller value found
+                    if value != 0 and value < smallest:
+                        smallest = value
+                        next_index = np.array([new_y, new_x])
+                        # Check if goal start has been reached
+                        if smallest == 1:
+                            pathfinding = False
+                            continue
+            bfs_path.append(index)
 
-    # Convert list of arrays to 2D array with format [[y1, y2, y3, ... ,yn],[x1, x2, x3, ... ,xn]]
-    bfs_path.append(bfs_start)
-    fin_path = np.zeros([2, len(bfs_path)])
-    for i in range(len(bfs_path)):
-        fin_path[0, i] = bfs_path[i][0]
-        fin_path[1, i] = bfs_path[i][1]
-    print(fin_path)
+        # Convert list of arrays to 2D array with format [[y1, y2, y3, ... ,yn],[x1, x2, x3, ... ,xn]]
+        bfs_path.append(bfs_start)
+        fin_path = np.zeros([2, len(bfs_path)])
+        for i in range(len(bfs_path)):
+            fin_path[0, i] = bfs_path[i][0]
+            fin_path[1, i] = bfs_path[i][1]
+    else:
+        fin_path = None
 
-    return fin_path
+    return fin_path, solvable
---
+++ Example_Usage_Solvable_Map.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from BFS import bfs
+
+# Initialise map parameters for a solvable map
+map = np.zeros([25, 30])
+map[20, 0:10] = 1
+map[20, 19:30] = 1
+map[8, 4:20] = 1
+map[9:25, 16] = 1
+start = np.array([24, 1])
+goal = np.array([24, 29])
+
+# Run the BFS for both maps
+path, solvable = bfs(map, goal, start)
+
+# Check if path was found
+if solvable:
+    # Plot map, path, start and goal
+    plt.figure()
+    plt.imshow(map, cmap='gray', interpolation='nearest')
+    plt.plot(path[1, :], path[0, :], marker='o', color='red', label='Path')
+    plt.plot(start[1], start[0], marker='^', color='green', label='Start')
+    plt.plot(goal[1], goal[0], marker='v', color='green', label='Goal')
+    plt.legend()
+else:
+    # Plot map, start and goal
+    print("Map 1 is unsolvable")
+    plt.figure()
+    plt.imshow(map, cmap='gray', interpolation='nearest')
+    plt.plot(start[1], start[0], marker='^', color='green', label='Start')
+    plt.plot(goal[1], goal[0], marker='v', color='green', label='Goal')
+    plt.legend()
+
+plt.show()
